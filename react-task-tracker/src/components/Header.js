57a0e5da3333ab49a44fbfb89797c005bd6abae2
lily liu <lliu.lifei@gmail.com>
2021-02-15 00:20:19 -0600

    Initial changes: created the app and created header, button compponents

---
+++ react-task-tracker/src/components/Header.js
@@ -0,0 +1,20 @@
+import Button from './Button'
+
+const Header = (props) => {
+    const onClick = () => {
+        console.log('click')
+    }
+    return (
+        <header className = 'header'>
+            <h1> {props.title}</h1>
+            <Button color = 'green' text = 'Add' 
+            onClick = {onClick}/>
+        </header>
+    )
+}
+
+Header.defaultProps = {
+    title : "Task Tracker"
+}
+
+export default Header
